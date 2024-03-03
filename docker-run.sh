4917cc03d79e1cb4fd616ee2f3267bb6c9827f70
Andrew Fasano <fasano@mit.edu>
2024-03-02 22:19:28 -0500

    Log and unify results

--- docker-run.sh
+++ docker-run.sh
@@ -2,32 +2,45 @@
 
 set -eu
 
-
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <path to firmware file or dir>"
   exit 1
 fi
 
 IN_PATH=$(readlink -f "$1")
-IN_DIR=$(dirname "$IN_PATH")
-IN_FILE=$(basename "$IN_PATH")
 
-if [ -d "${IN_PATH}" ]; then
-    # Process the whole dir
-    IN_DIR="${IN_PATH}"
-    IN_FILE=""
+if [ ! -d "${IN_PATH}" ]; then
+  echo "${IN_PATH} is not a directory."
+  exit 1
 fi
 
+# Define the file where results will be stored
+RESULTS_FILE="docker_results.txt"
+
+# Function to process a single file and append the result to the results file
+process_file() {
+    local file_path="$1"
+    local in_dir=$(dirname "$file_path")
+    local in_file=$(basename "$file_path")
+    local result_file="${in_dir}/${in_file}_result.txt"
 
 
-docker run --rm -it -v /dev:/dev \
-    -v ${IN_DIR}:/work/firmwares \
-    --privileged \
-    fcore \
-        bash -c "\
-            cd /work/FirmAE && \
-            sudo service postgresql start && \
-            ./run.sh -c brand \"/work/firmwares/${IN_FILE}\"; \
-            echo -n "RESULT:"; \
-            cat ./scratch/1/result \
-            "
+    docker run --rm -v /dev:/dev \
+        -v "${in_dir}:/work/firmwares" \
+        --privileged \
+        fcore \
+            bash -c "\
+                cd /work/FirmAE && \
+                sudo service postgresql start && \
+                ./run.sh -c brand \"/work/firmwares/${in_file}\"; \
+                echo -n 'RESULT: '; \
+                cat ./scratch/*/result \
+                " | tee $result_file
+    grep 'RESULT: ' $result_file | cut -d' ' -f1 >> $RESULTS_FILE # This could get clobbered with races
+}
+
+export -f process_file
+export RESULTS_FILE
+
+# Find all files in the directory and pass them to xargs for parallel processing
+find "${IN_PATH}" -type f -print0 | xargs -0 -I {} -P $(nproc) bash -c 'process_file "$@"' _ {}
